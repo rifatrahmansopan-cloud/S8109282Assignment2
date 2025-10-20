a695849ba01012ee130edc15d0ddf0ece5b00759
rifatrahmansopan-cloud <rifatrahmansopan@gmail.com>
2025-10-21 01:35:54 +1100

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "S8109282Assignment2"
+include(":app")
+
+
+ 
